4171fd3047bf1c2d6e5f72634080224eb3ace3b9
valerianofilipec <valeriofilipec@gmail.com>
2023-11-24 22:42:52 +0000

    add features updade,destroy dev

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -31,4 +31,38 @@ export default{
         }
         return response.json(dev)
     },
+
+    async update(req, res) {
+        const { id } = req.params;
+        const { techs, latitude, longitude, bio, name} = req.body;
+
+        const dev = await Dev.findById(id);
+        if (!dev){
+          return res.status(400).json({error: 'Dev não foi encontrado!'})
+        }
+    
+        const techsArray = parseStringAsArray(techs);
+        const location = {
+          type: 'Point',
+          coordinates: [longitude, latitude]
+        };
+    
+        const devUpdated = await dev.updateOne({
+            name,
+            bio,
+            techs: techsArray,
+            location
+        });
+        return res.json(devUpdated);
+      },
+    
+      async destroy(req, res){
+        const {id} = req.params;
+        const dev = await Dev.findById(id);
+        if(!dev){
+          return res.status(400).json({error: 'Dev não foi encontrado!'});
+        }
+        await dev.remove();
+        res.json({message: "Dev removido com sucesso!"})
+      }
 }
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -15,7 +15,8 @@ const routes = Router();
 
 routes.get('/devs', DevController.index)
 routes.post('/devs', DevController.store)
-//routes.delete('devs', DevController.destroy)
 routes.get('/search', SearchController.index)
+routes.put('/devs/:id', DevController.update);
+routes.delete('/devs/:id', DevController.destroy);
 
 export {routes};
